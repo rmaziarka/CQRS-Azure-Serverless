cf1323a786a8e81752d18ebfd30d2338e4a793bd
Radek Maziarka <rmaziarka@pgs-soft.com>
2019-10-23 17:35:34 +0200

    Add CosmosDB ARM

--- scripts/infrastructure/scripts.sh
+++ scripts/infrastructure/scripts.sh
@@ -1,3 +1,7 @@
 az group create --name CQRS-Azure-Serverless --location WestEurope
 
+az keyvault create -n cqrsazureserverless -g CQRS-Azure-Serverless
+
+az group deployment create --resource-group CQRS-Azure-Serverless --template-file cosmosdb.json
+
 az group deployment create --resource-group CQRS-Azure-Serverless --template-file functionapp.json
