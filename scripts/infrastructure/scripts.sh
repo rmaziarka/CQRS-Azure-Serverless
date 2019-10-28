156dc9bfcc4aa54c2eb063c968e807b762246730
Radek Maziarka <rmaziarka@pgs-soft.com>
2019-10-28 11:48:11 +0100

    Pull out storage account creation to another ARM Add access from real estate function to other services by Key Vault Clean up excessive parameters

--- scripts/infrastructure/scripts.sh
+++ scripts/infrastructure/scripts.sh
@@ -1,6 +1,10 @@
+
+
 az group create --name CQRS-Azure-Serverless --location WestEurope
 
 az keyvault create -n cqrsazureserverless -g CQRS-Azure-Serverless
+
+az group deployment create --resource-group CQRS-Azure-Serverless --template-file storageAccount.json
 
 az group deployment create --resource-group CQRS-Azure-Serverless --template-file cosmosdb.json
 
