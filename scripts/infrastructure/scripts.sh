cd7bb640172835f310b3f82343c13b7611fd9db5
Radek Maziarka <rmaziarka@pgs-soft.com>
2019-12-13 14:28:32 +0100

    - Scripts - move values to parameters - ResourceGroup - move script to template - KeyVault - move script to template - CosmosDB - remove parameters

--- scripts/infrastructure/scripts.sh
+++ scripts/infrastructure/scripts.sh
@@ -1,11 +1,14 @@
+# usage: sh scripts.sh CQRS-Azure-Serverless NorthEurope
 
+resourceGroupName=$1
+resourceGroupLocation=$2
 
-az group create --name CQRS-Azure-Serverless --location WestEurope
+az deployment create -l $resourceGroupLocation --template-file resourceGroup.json --parameters resourceGroupName=$resourceGroupName
 
-az keyvault create -n cqrsazureserverless -g CQRS-Azure-Serverless
+az group deployment create --resource-group $resourceGroupName --template-file keyVault.json
 
-az group deployment create --resource-group CQRS-Azure-Serverless --template-file storageAccount.json
+az group deployment create --resource-group $resourceGroupName --template-file storageAccount.json
 
-az group deployment create --resource-group CQRS-Azure-Serverless --template-file cosmosdb.json
+az group deployment create --resource-group $resourceGroupName --template-file cosmosdb.json
 
-az group deployment create --resource-group CQRS-Azure-Serverless --template-file functionapp.json
+az group deployment create --resource-group $resourceGroupName --template-file functionapp.json
