8fc18e9087e355a68fe28fce42b3f65f4abd5237
Radek Maziarka <maziarka.radoslaw@outlook.com>
2020-02-16 23:11:50 +0100

    Return book funcion

---
+++ LibraryCorp/Funcs/ReturnBook/ReturnBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryCorp.Funcs.ReturnBook
+{
+    public class ReturnBook
+    {
+        public String ReaderId { get; set; }
+
+        public string BorrowId { get; set; }
+
+        public string LibraryId { get; set; }
+    }
+}
---
+++ LibraryCorp/Funcs/ReturnBook/ReturnBookFunc.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading.Tasks;
+using LibraryCorp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace LibraryCorp.Funcs.ReturnBook
+{
+    public static class ReturnBookFunc
+    {
+        [FunctionName("ReturnBook")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "returnBook")] ReserveBook.ReserveBook command,
+            ILogger log)
+        {
+            try
+            {
+                var repo = new CosmosRepo(command.LibraryId);
+                repo.StartTransaction();
+
+                var copyToReserve = await repo.GetFreeCopy(command.BrandId);
+                copyToReserve.Block();
+                
+                var reservation = new Reservation(command.ReaderId, copyToReserve.Id);
+                repo.Create(reservation);
+                await repo.ExecuteAsync();
+
+                return new OkObjectResult(new { reservationId = reservation.Id });
+
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, e.StackTrace);
+
+                throw;
+            }
+        }
+    }
+}
